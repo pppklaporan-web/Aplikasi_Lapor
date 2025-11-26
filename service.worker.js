b86909859df661ae492a2a2a5e4594f8d540844e
Ryan Oktarino <pppklaporan@gmail.com>
2025-11-26 14:43:10 +0700

    Update service.worker.js

--- service.worker.js
+++ service.worker.js
@@ -1,21 +1,8 @@
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("v1").then(cache => {
-      return cache.addAll([
-        "login.html",
-        "laporan.html",
-        "dashboard.html",
-        "style.css",
-        "manifest.json"
-      ]);
-    })
-  );
+self.addEventListener("install", event => {
+    self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(res => {
-      return res || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", event => {
+    event.respondWith(fetch(event.request));
 });
+
