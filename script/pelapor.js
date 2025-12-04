47da1908879528447bd0e36b1f8087d5678e78a1
Ryan Oktarino <pppklaporan@gmail.com>
2025-12-04 20:59:29 +0700

    Add files via upload

---
+++ script/pelapor.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("laporForm");
+  const statusEl = document.getElementById("status");
+
+  // GANTI URL BERIKUT DENGAN URL APPS SCRIPT ANDA
+  const API_URL = "https://script.google.com/macros/s/AKfycbye-HYr7mlljZBqn5ucGfAXUGx3UN_7PHuLsuc3maPchJ3d9rnrl_Io6Oq5FUw50eee8Q/exec";
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    statusEl.innerHTML = "Mengirim laporan...";
+
+    const nama = document.getElementById("nama").value;
+    const ruangan = document.getElementById("ruangan").value;
+    const keterangan = document.getElementById("keterangan").value;
+    const fotoInput = document.getElementById("foto");
+
+    let fotoBase64 = ""; // default
+
+    // Jika ada foto → ubah ke base64
+    if (fotoInput.files.length > 0) {
+      fotoBase64 = await toBase64(fotoInput.files[0]);
+    }
+
+    const payload = {
+      nama,
+      ruangan,
+      keterangan,
+      foto: fotoBase64
+    };
+
+    try {
+      const res = await fetch(API_URL, {
+        method: "POST",
+        body: JSON.stringify(payload)
+      });
+
+      const result = await res.json();
+
+      if (result.status === "success") {
+        statusEl.innerHTML = "Laporan berhasil dikirim ✔";
+        form.reset();
+      } else {
+        statusEl.innerHTML = "Gagal: " + result.message;
+      }
+
+    } catch (err) {
+      console.error(err);
+      statusEl.innerHTML = "Terjadi kesalahan jaringan!";
+    }
+  });
+});
+
+
+function toBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = reject;
+  });
+}
