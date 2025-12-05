aa0f8abe8a0046b57f00d23e65d0f78fb52bd784
Ryan Oktarino <pppklaporan@gmail.com>
2025-12-05 22:02:58 +0700

    Update dashboard.js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,5 +1,4 @@
-const GAS_URL = "[https://script.google.com/macros/s/AKfycbxYCbIQWF1cMCXEHXg8iEREhbMhqZXiPj58Vtb-neJjDrCAmk4F4jkOYhtGR87TGy7ubw/exec](https://script.google.com/macros/s/AKfycbxYCbIQWF1cMCXEHXg8iEREhbMhqZXiPj58Vtb-neJjDrCAmk4F4jkOYhtGR87TGy7ubw/exec)";
-
+const GAS_URL = "(https://script.google.com/macros/s/AKfycbxYCbIQWF1cMCXEHXg8iEREhbMhqZXiPj58Vtb-neJjDrCAmk4F4jkOYhtGR87TGy7ubw/exec)"
 document.addEventListener('DOMContentLoaded', () => {
 const listWrap = document.getElementById('listWrap');
 const loading = document.getElementById('loading');
@@ -91,3 +90,4 @@ search.addEventListener('input', fetchReports);
 
 fetchReports();
 });
+
